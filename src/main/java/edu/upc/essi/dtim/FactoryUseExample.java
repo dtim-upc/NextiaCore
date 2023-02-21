a49445dbe13a36e847db3bacee088aedc2393335
Víctor Asenjo <victorasenj@gmail.com>
2023-02-21 18:40:30 +0100

    Update FactoryUseExample.java

--- src/main/java/edu/upc/essi/dtim/FactoryUseExample.java
+++ src/main/java/edu/upc/essi/dtim/FactoryUseExample.java
@@ -1,24 +1,30 @@
 package edu.upc.essi.dtim;
 
+import java.util.List;
+import java.util.Map;
+
 public class FactoryUseExample {
-    GraphStoreFactory rdfModel = RDFModelFactory.getInstance();
 
-    // Inicializar el modelo
-    rdfModel.initModel();
 
-    // Añadir una tripla
-    rdfModel.addTriple("http://example.org/resource1", "http://example.org/predicate1", "Valor de objeto 1");
+    public void useExample(){
+        GraphStoreInterface graphStoreInstance = GraphStoreFactory.getInstance();
+        // Inicializar el graph
+        graphStoreInstance.initGraph();
+
+        // Añadir una tripla
+        graphStoreInstance.addTriple("http://example.org/resource1", "http://example.org/predicate1", "Valor de objeto 1");
 
         // Consultar el modelo con una consulta SPARQL
         String sparqlQuery = "SELECT ?s ?p ?o WHERE { ?s ?p ?o }";
-        List<Map<String, String>> resultList = rdfModel.query(sparqlQuery);
-    for (Map<String, String> row : resultList) {
+        List<Map<String, String>> resultList = graphStoreInstance.query(sparqlQuery);
+        for(Map<String, String> row:resultList) {
             System.out.println(row.get("s") + " " + row.get("p") + " " + row.get("o"));
         }
 
-    // Eliminar una tripla
-    rdfModel.removeTriple("http://example.org/resource1", "http://example.org/predicate1", "Valor de objeto 1");
+        // Eliminar una tripla
+        graphStoreInstance.removeTriple("http://example.org/resource1", "http://example.org/predicate1", "Valor de objeto 1");
 
-    // Cerrar el modelo
-    rdfModel.closeModel();
+        // Cerrar el modelo
+        graphStoreInstance.closeModel();
+    }
 }
