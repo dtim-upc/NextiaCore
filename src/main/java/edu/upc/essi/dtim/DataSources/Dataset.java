cbbbac39cb7ff04dade622c5c08e4c684d6d0bc2
Víctor Asenjo <victorasenj@gmail.com>
2023-03-29 21:49:34 +0200

    cleanning Dataset and Attribute code

--- src/main/java/edu/upc/essi/dtim/DataSources/Dataset.java
+++ src/main/java/edu/upc/essi/dtim/DataSources/Dataset.java
@@ -1,14 +1,18 @@
 package edu.upc.essi.dtim.DataSources;
 
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.security.Timestamp;
 
+@Getter
+@Setter
 public class Dataset {
 
 	private String datasetId;
 	private String name;
 	private String description;
-	private DatasetTypes type;
 	private Timestamp created_at;
 
 	/**
@@ -19,65 +23,4 @@ public class Dataset {
 		// TODO - implement Dataset.Dataset
 		throw new UnsupportedOperationException();
 	}
-
-	public String getDatasetId() {
-		return this.datasetId;
-	}
-
-	/**
-	 *
-	 * @param datasetId
-	 */
-	public void setDatasetId(String datasetId) {
-		this.datasetId = datasetId;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	/**
-	 *
-	 * @param name
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return this.description;
-	}
-
-	/**
-	 *
-	 * @param description
-	 */
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public DatasetTypes getType() {
-		return this.type;
-	}
-
-	/**
-	 *
-	 * @param type
-	 */
-	public void setType(DatasetTypes type) {
-		this.type = type;
-	}
-
-	public Timestamp getCreated_at() {
-		return this.created_at;
-	}
-
-	/**
-	 *
-	 * @param created_at
-	 */
-	public void setCreated_at(Timestamp created_at) {
-		this.created_at = created_at;
-	}
-
 }
--- src/main/java/edu/upc/essi/dtim/Discovery/Attribute.java
+++ src/main/java/edu/upc/essi/dtim/Discovery/Attribute.java
@@ -1,35 +1,13 @@
 package edu.upc.essi.dtim.Discovery;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.*;
-
+@Getter
+@Setter
 public class Attribute {
-
 	Collection<Alignment> alignment;
 	private String name;
 	private String type;
-
-	public String getName() {
-		return this.name;
-	}
-
-	/**
-	 *
-	 * @param name
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getType() {
-		return this.type;
-	}
-
-	/**
-	 *
-	 * @param type
-	 */
-	public void setType(String type) {
-		this.type = type;
-	}
-
 }
