a59833635739580f2f881a0b1a44c2f2a72793dc
Víctor Asenjo <victorasenj@gmail.com>
2023-03-29 21:38:04 +0200

    cleanning Triple code

--- src/main/java/edu/upc/essi/dtim/Graph/Triple.java
+++ src/main/java/edu/upc/essi/dtim/Graph/Triple.java
@@ -1,48 +1,11 @@
 package edu.upc.essi.dtim.Graph;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class Triple {
-
 	URI predicate;
 	URI subject;
 	URI object;
-
-	public URI getSubject() {
-		return this.subject;
-	}
-
-	public URI getPredicate() {
-		return this.predicate;
-	}
-
-	public URI getObject() {
-		return this.object;
-	}
-
-	/**
-	 * 
-	 * @param URIsubject
-	 */
-	public void setSubject(String URIsubject) {
-		this.subject = new URI(URIsubject);
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param URIpredicate
-	 */
-	public void setPredicate(String URIpredicate) {
-		// TODO - implement Triple.setPredicate
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param URIobject
-	 */
-	public void setObject(String URIobject) {
-		// TODO - implement Triple.setObject
-		throw new UnsupportedOperationException();
-	}
-
 }
