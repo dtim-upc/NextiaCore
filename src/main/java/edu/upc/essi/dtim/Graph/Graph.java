afca086ddb0eeb012a9b02fd65a9d4c674828e8a
Víctor Asenjo <victorasenj@gmail.com>
2023-04-07 18:32:41 +0200

    Update Graph.java

--- src/main/java/edu/upc/essi/dtim/Graph/Graph.java
+++ src/main/java/edu/upc/essi/dtim/Graph/Graph.java
@@ -1,12 +1,17 @@
 package edu.upc.essi.dtim.Graph;
 
 import edu.upc.essi.dtim.Queries.QueryResult;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.*;
-
+@Getter
+@Setter
 public abstract class Graph {
 
 	Collection<QueryResult> queryResult;
 	private int name;
+
+	private String NextiaCoreAsenjoImplementationTry;
 
 }
