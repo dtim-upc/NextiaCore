10e0f2e2615a447f43afa055a299b7e0c7cf5290
Víctor Asenjo <victorasenj@gmail.com>
2023-04-14 11:08:29 +0200

    Graph not abstract

--- src/main/java/edu/upc/essi/dtim/GraphProcessing/GraphProcessingImpl.java
+++ src/main/java/edu/upc/essi/dtim/GraphProcessing/GraphProcessingImpl.java
@@ -8,11 +8,6 @@ import org.apache.jena.graph.Triple;
 public class GraphProcessingImpl implements GraphProcessingInterface{
     private Graph graph;
 
-    public GraphProcessing() {
-        // Creamos un nuevo grafo en memoria usando Jena In-Memory
-
-    }
-
     // Método para agregar triples al grafo
     public void addTriple(Node subject, Node predicate, Node object) {
         graph.add(new Triple(subject, predicate, object));
