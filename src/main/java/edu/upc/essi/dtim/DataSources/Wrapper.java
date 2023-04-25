de914b932cd39fe45c3d6729657dffdc18a87aa1
Víctor Asenjo <victorasenj@gmail.com>
2023-04-25 15:27:32 +0200

    passing tests

--- src/main/java/edu/upc/essi/dtim/DataSources/Wrapper.java
+++ src/main/java/edu/upc/essi/dtim/DataSources/Wrapper.java
@@ -3,28 +3,4 @@ package edu.upc.essi.dtim.DataSources;
 import edu.upc.essi.dtim.DataSources.dataset.Dataset;
 
 public class Wrapper {
-
-	/**
-	 * 
-	 * @param dataset
-	 */
-	public Wrapper(Dataset dataset) {
-		// TODO - implement Wrapper.Wrapper
-		throw new UnsupportedOperationException();
-	}
-
-	public void getDataset() {
-		// TODO - implement Wrapper.getDataset
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param dataset
-	 */
-	public void setDataset(Dataset dataset) {
-		// TODO - implement Wrapper.setDataset
-		throw new UnsupportedOperationException();
-	}
-
 }
--- src/main/java/edu/upc/essi/dtim/Graph/GlobalGraph.java
+++ src/main/java/edu/upc/essi/dtim/Graph/GlobalGraph.java
@@ -1,30 +1,18 @@
 package edu.upc.essi.dtim.Graph;
 
+
 public class GlobalGraph extends Graph {
 
 	IntegratedGraph extended;
 	private IntegratedGraph integratedGraph;
 
 	/**
-	 * 
-	 * @param integratedGraph
+	 * getIntegratedGraph
+	 * @param integratedGraph IntegratedGraph
 	 */
 	public GlobalGraph(IntegratedGraph integratedGraph) {
 		super(integratedGraph.getName(), integratedGraph.getTriples());
 		// TODO - implement GlobalGraph.GlobalGraph
-		throw new UnsupportedOperationException();
-	}
-
-	public IntegratedGraph getIntegratedGraph() {
-		return this.integratedGraph;
-	}
-
-	/**
-	 * 
-	 * @param integratedGraph
-	 */
-	public void setIntegratedGraph(IntegratedGraph integratedGraph) {
-		// TODO - implement GlobalGraph.setIntegratedGraph
 		throw new UnsupportedOperationException();
 	}
 
