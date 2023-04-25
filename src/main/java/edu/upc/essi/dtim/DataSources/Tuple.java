2a627ddb995041a467361c8a88482ccedaef8f23
Víctor Asenjo <victorasenj@gmail.com>
2023-04-25 15:22:33 +0200

    Update Tuple.java

--- src/main/java/edu/upc/essi/dtim/DataSources/Tuple.java
+++ src/main/java/edu/upc/essi/dtim/DataSources/Tuple.java
@@ -1,30 +1,4 @@
 package edu.upc.essi.dtim.DataSources;
 
-import java.util.List;
-
 public class Tuple {
-
-	/**
-	 * 
-	 * @param values
-	 */
-	public Tuple(List<String> values) {
-		// TODO - implement Tuple.Tuple
-		throw new UnsupportedOperationException();
-	}
-
-	public void getValues() {
-		// TODO - implement Tuple.getValues
-		throw new UnsupportedOperationException();
-	}
-
-	/**
-	 * 
-	 * @param values
-	 */
-	public void setValues(List<String> values) {
-		// TODO - implement Tuple.setValues
-		throw new UnsupportedOperationException();
-	}
-
 }
