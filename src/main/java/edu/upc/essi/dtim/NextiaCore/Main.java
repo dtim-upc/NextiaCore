2ba8bc410db738343178c43e865a7fd27a19461e
Marc Maynou <marcmaynou99@gmail.com>
2023-12-01 17:38:55 +0100

    Added APIDataset

---
+++ src/main/java/edu/upc/essi/dtim/NextiaCore/Main.java
@@ -0,0 +1,18 @@
+package edu.upc.essi.dtim.NextiaCore;
+
+import edu.upc.essi.dtim.NextiaCore.datasources.dataRepository.RelationalJDBCRepository;
+import edu.upc.essi.dtim.NextiaCore.datasources.dataset.SQLDataset;
+import edu.upc.essi.dtim.NextiaCore.discovery.Attribute;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        /*SQLDataset d = new SQLDataset("personas", "personas", "", "personas", "dtim.essi.upc.edu", "5432", "vasenjo", "jBGRfEu", "jdbc:postgresql://dtim.essi.upc.edu:5432/odin_test");
+
+        List<Attribute> atts = d.getAttributes();
+        for (Attribute a: atts) {
+            System.out.println(a.getName());
+        }*/
+    }
+}
